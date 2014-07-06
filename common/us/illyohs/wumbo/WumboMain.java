daabddca1d0fb2c925550f67506f2d639b0bd5d7
Anthony Anderson <LordIllyohs@gmail.com>
2014-07-06 15:47:22 -0400

    Inital commit
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

---
+++ common/us/illyohs/wumbo/WumboMain.java
@@ -0,0 +1,22 @@
+/**
+ * This class was created by Anthony Anderson(Lord Illyohs)
+ * It is distributed as part of Wumbo
+ * Source Code: https://github.com/LordIllyohs/Wumbo
+ *
+ * Wumbo is Open Source and distributed under a
+ * Attribution-NonCommercial-ShareAlike 3.0 Unported license.
+ * (http://creativecommons.org/licenses/by-nc-sa/4.0/)
+ *
+ * 
+ * Class created on Jul 6, 2014 at 11:44:37 AM
+ * 
+ */
+package us.illyohs.wumbo;
+
+/**
+ * @author anthony
+ *
+ */
+public class WumboMain {
+
+}
